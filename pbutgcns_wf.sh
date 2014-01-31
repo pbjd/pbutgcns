964b6a4485ccafca0a20089827217e8b6dfbeef9
Jim "Double Phoenix" Drake <jdrake@pacificbiosciences.com>
2014-01-31 15:37:15 -0800

    Bug 24663. Bug 24430. Add some verbosity to workflow script and make it resilient to empty input files (for when we chunk).

--- pbutgcns_wf.sh
+++ pbutgcns_wf.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# always remove the tmp dir after exiting
 trap "rm -rf $tmp; popd" EXIT SIGINT
+
 
 # generate files in local temp
 pushd $tmp
 for u in $(awk '{print $1}' $utg)
 do
+    set -x
     tigStore-adapter.py $cap $u || exit $?
-    pbutgcns unitig_${u}.lay >> cns.fa || exit $?
+    pbutgcns -j ${nproc-1} unitig_${u}.lay >> cns.fa || exit $?
+    set +x
 done
 
-mv cns.fa $cns || exit $?
+# sometimes, the utg file will be empty :-/, sooo no consensus
+[ ! -s $utg ] && echo "Skipping empty input: $utg"
+if [ -e cns.fa ]
+then
+    set -x
+    mv cns.fa $cns
+fi
